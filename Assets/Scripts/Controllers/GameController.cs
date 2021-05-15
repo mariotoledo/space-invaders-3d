9e18ab9d221682e8579cdb60664ffeab57907235
Mario Toledo <mariotoledo12@gmail.com>
2021-05-15 20:21:48 -0300

    creating initial game

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float spaceShipSpeed = 0.01f;
+
+    private float minScreenBound;
+    private float maxScreenBound;
+
+    public Player player;
+    public Camera camera;
+
+    void Start() {
+        CalculateScreenBounds();
+    }
+
+    void Update()
+    {
+        UpdatePlayerMovement();
+    }
+
+    /*
+        Calculates screen bounds based on Screen height for 1:1 perspective
+    */
+    void CalculateScreenBounds() {
+        float middleScreenPoint = Screen.width / 2;
+
+        minScreenBound = middleScreenPoint - (Screen.height / 2);
+        maxScreenBound = middleScreenPoint + (Screen.height / 2);
+    }
+
+    void UpdatePlayerMovement() {
+        float horizontalTrigger = Input.GetAxis("Horizontal");
+
+        float playerPositionScreenX = camera.WorldToScreenPoint(player.GetPosition()).x;
+
+        if((playerPositionScreenX < minScreenBound && horizontalTrigger < 0) || (playerPositionScreenX > maxScreenBound && horizontalTrigger > 0)) {
+            horizontalTrigger = 0;
+        }
+
+        player.SetPosition(player.GetPosition() + (Vector3.right * horizontalTrigger * spaceShipSpeed));
+    }
+}
---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform playerPosition;
+
+    void Start()
+    {
+        playerPosition = GetComponent<Transform>();
+    }
+
+    public Vector3 GetPosition() {
+        return playerPosition.position;
+    }
+
+    public void SetPosition(Vector3 position) {
+        playerPosition.position = position;
+    }
+}
